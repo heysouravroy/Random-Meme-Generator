11d01d258a692b94cdcc9c50bdce1afd3bb0aecc
Sourav Roy <heysouravroy@gmail.com>
2024-03-29 13:36:56 +0530

    Update logic.js

--- logic.js
+++ logic.js
@@ -1,7 +1,7 @@
 const elem = document.getElementById('meme')
 
 function getMeme(){
-    fetch('https://meme-api.herokuapp.com/gimme/ProgrammerHumor')
+    fetch('https://meme-api.com/gimme')
     .then(res => res.json())
     .then(data => {
         elem.src = data.url
